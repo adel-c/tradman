fea7734cd2f915e6ebcb41d5051b5b7ec3dc8ce5
Adel <576853-astran@users.noreply.gitlab.com>
2023-09-09 14:44:33 +0200

    test export

--- rest-api/src/main/java/com/ace/tradman/rest/RestApiController.java
+++ rest-api/src/main/java/com/ace/tradman/rest/RestApiController.java
@@ -3,6 +3,9 @@ package com.ace.tradman.rest;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 @RestController
@@ -10,19 +13,30 @@ import java.util.Map;
 @AllArgsConstructor
 public class RestApiController {
 
-    @GetMapping("/get")
-    public String main( @RequestParam Map<String, String> allRequestParams
-    ) {
+    @GetMapping("/download-file")
+    public void main( HttpServletResponse response
+    ) throws IOException {
 
 
+        String csvFileName = "books.csv";
 
-        return
-        //language=json
-    """
-            {
-      "machin": true,
-      "truc": "asddd"
-    }""";
+        response.setContentType("text/csv");
+
+        // creates mock data
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"",
+                csvFileName);
+        response.setHeader(headerKey, headerValue);
+
+
+        PrintWriter writer = response.getWriter();
+       writer.println("""
+    machin;truc;much
+    v1;v2;v3
+    v4;v5;v6
+    """);
+       writer.close();
+
     }
 
 

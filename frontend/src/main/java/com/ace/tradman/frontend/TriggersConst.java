c4ce1ee18ef1d63669d0ea3d10b04633c628e2bc
Adel <576853-astran@users.noreply.gitlab.com>
2023-09-09 16:33:22 +0200

    clean up

--- frontend/src/main/java/com/ace/tradman/frontend/TriggersConst.java
+++ frontend/src/main/java/com/ace/tradman/frontend/TriggersConst.java
@@ -2,5 +2,5 @@ package com.ace.tradman.frontend;
 
 public class TriggersConst {
 
-    public static final String RELOAD_TRANSLATION_TABLE="RELOAD_TRANSLATION_TABLE";
+    public static final String RELOAD_TRANSLATION_TABLE="translation_reload_translation_table";
 }

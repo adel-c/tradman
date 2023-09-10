a7b296d5fe76d956c2128d16e0dd27ee9449eb9a
Adel <576853-astran@users.noreply.gitlab.com>
2023-09-10 21:17:44 +0200

    add sort class

--- core/src/main/java/com/ace/tradman/translation/SearchTranslationQuery.java
+++ core/src/main/java/com/ace/tradman/translation/SearchTranslationQuery.java
@@ -9,10 +9,37 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class SearchTranslationQuery {
+    enum Sort {
+        ASC,
+        DESC,
+        NONE;
+
+        public Sort toggle() {
+            return switch (this) {
+                case NONE, ASC -> DESC;
+                case DESC -> ASC;
+            };
+        }
+    }
 
     List<String> partners;
     List<String> countries;
     List<String> profiles;
     List<String> languages;
     String key;
+
+
+    @Getter
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class QuerySort {
+        private Sort partner;
+        private Sort country;
+        private Sort profile;
+        private Sort language;
+        private Sort key;
+        private Sort value;
+    }
 }

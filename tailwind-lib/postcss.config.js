0046c6f607d35d8f37d16ea7fb5e9cd59704c47e
Adel <576853-astran@users.noreply.gitlab.com>
2023-09-12 23:17:18 +0200

    config with npm

--- tailwind-lib/postcss.config.js
+++ tailwind-lib/postcss.config.js
@@ -4,11 +4,3 @@ module.exports = {
         autoprefixer: {},
     }
 }
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{html,js}","../frontend/src/main/resources/templates/**/*.{html,js,ftlh}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
